c962ec87e369a19817d524a55f78fe71f099af01
Josh Padilla <jdp237@nau.edu>
2021-03-30 13:42:34 -0700

    updating 'about' section and some styling

--- myportfolio/src/components/About/about.js
+++ myportfolio/src/components/About/about.js
@@ -3,93 +3,107 @@ import { Paper, Grid } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Typed from "react-typed";
 import Dilla from "../../assets/images/dilla.png";
+import CertImg from "../../assets/images/cert.png";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    width: "50%",
-    margin: "auto",
-  },
-  header: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    backgroundColor: "transparent",
-    marginTop: "5%",
-    fontSize: "35px",
-  },
-  container: {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    // width: "100vw",
-    textAlign: "center",
-    zIndex: 1,
-  },
+    root: {
+        flexGrow: 1,
+        width: "50%",
+        margin: "auto",
+    },
+    header: {
+        padding: theme.spacing(2),
+        textAlign: "center",
+        backgroundColor: "transparent",
+        marginTop: "5%",
+        fontSize: "35px",
+    },
+    container: {
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        // width: "100vw",
+        textAlign: "center",
+        zIndex: 1,
+    },
+    cert: {
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+    },
+    certImg: {
+        maxWidth: "75%"
+    }
 }));
 
 const About = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <Grid>
-        <Grid item sm={12}>
-          <Paper className={classes.header}>
-            <Typed strings={["About Me"]} typeSpeed={40} />
-          </Paper>
-        </Grid>
-        <Grid>
-          <p>
-            <img src={Dilla} alt="Dilla" id="dilla" />
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <Grid>
+                <Grid item sm={12}>
+                    <Paper className={classes.header}>
+                        <Typed strings={["About Me"]} typeSpeed={40} />
+                    </Paper>
+                </Grid>
+                <Grid>
+                    <p>
+                        <Grid item lg={6} sm={12}>
+                            <img src={Dilla} alt="Dilla" id="dilla" sm={12} />
+                        </Grid>
             Hello! My name is Josh Padilla.
             <br />
-            <br />
+                        <br />
             I am 28 years old and currently reside in Phoenix, Arizona. <br />
-            <br /> I am a Full-Stack Web Developer, with my specialty being the
-            MERN stack. I've been studying software engineering through the
-            University of Arizona for the past 6 months and have come to realize
-            it as one of my passions. I thoroughly enjoy making computers do
-            cool things and giving life to applications and websites. React has
-            been my go-to framework for javascript as I find it very powerful
-            and fun to code with.
+                        <br /> I am a Full-Stack Web Developer, with my specialty being the
+            MERN stack. I've been studying software engineering for the past year and have recently been certified as a Full Stack Web Developer through the University of Arizona.
             <br />
+                        <br />
+                        <Grid item lg={4} sm={12} className={classes.cert}>
+                            <img src={CertImg} alt="certImg" id="cert" className={classes.certImg} />
+                        </Grid>
+                        <br />
+                        <br />
+            I thoroughly enjoy making computers and websites do cool things and love giving users a quality and fun experience online.
             <br />
+                        <br />
             Some of my other passions include hiking/backpacking (especially
             with my dog, Kenai), Photography, going to concerts (when there
             isn't a world-wide pandemic), and travelling (also when there isn't
             a world-wide pandemic going on). Check out my socials below to see
             some more of me! 👇👇
             <br />
-            <br />
-          </p>
-          <div className="social-links">
-            <a
-              href="https://www.instagram.com/j_dillaa/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <i className="fa fa-instagram" aria-hidden="true" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/joshua-padilla-4052371ab/"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <i className="fa fa-linkedin-square" aria-hidden="true" />
-            </a>
+                        <br />
+                    </p>
+                    <div className="social-links">
+                        <a
+                            href="https://www.instagram.com/j_dillaa/"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <i className="fa fa-instagram" aria-hidden="true" />
+                        </a>
+                        <a
+                            href="https://www.linkedin.com/in/joshua-padilla-4052371ab/"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <i className="fa fa-linkedin-square" aria-hidden="true" />
+                        </a>
 
-            <a
-              href="https://github.com/Jdilla1212"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              <i className="fa fa-github-square" aria-hidden="true" />
-            </a>
-          </div>
-        </Grid>
-      </Grid>
-    </div>
-  );
+                        <a
+                            href="https://github.com/Jdilla1212"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <i className="fa fa-github-square" aria-hidden="true" />
+                        </a>
+                    </div>
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
 
 export default About;
--- myportfolio/src/components/Resume/resume.js
+++ myportfolio/src/components/Resume/resume.js
@@ -12,123 +12,123 @@ import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import SchoolIcon from '@material-ui/icons/School';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    width: "50%",
-    margin: "auto"
-  },
-  header: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    backgroundColor: "transparent",
-    marginTop: "5%",
-    fontSize: "35px",
+    root: {
+        flexGrow: 1,
+        width: "50%",
+        margin: "auto"
+    },
+    header: {
+        padding: theme.spacing(2),
+        textAlign: "center",
+        backgroundColor: "transparent",
+        marginTop: "5%",
+        fontSize: "35px",
 
-  },
-  resumeContents: {
-    padding: theme.spacing(2),
-    textAlign: "center",
-    backgroundColor: "transparent",
-    margin: "2%",
-  },
+    },
+    resumeContents: {
+        padding: theme.spacing(2),
+        textAlign: "center",
+        backgroundColor: "transparent",
+        margin: "2%",
+    }
 }));
 
 const Resume = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.root}>
-      <Grid container spacing={2}>
-        <Grid item sm={12}>
-          <Paper className={classes.header}><Typed strings={["Resume"]} typeSpeed={40} /></Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={6}>
-          <Paper className={classes.resumeContents}>
-              <BusinessCenterIcon />
-              <h2>Work Experience</h2>
-              <Timeline style={{marginLeft: "-80%"}}>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-            <h4 style={{ marginTop: "0%"}}>Physical Therapy Technician - Chandler Physical Therapy</h4><p style={{ fontSize: "12px"}}>2020-present</p>
-            <p>
-            Responsible for assisting physical therapists with non-clinical tasks while caring for individuals
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={2}>
+                <Grid item sm={12}>
+                    <Paper className={classes.header}><Typed strings={["Resume"]} typeSpeed={40} /></Paper>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6}>
+                    <Paper className={classes.resumeContents}>
+                        <BusinessCenterIcon />
+                        <h2>Work Experience</h2>
+                        <Timeline style={{ marginLeft: "-80%" }}>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <h4 style={{ marginTop: "0%" }}>Physical Therapy Technician - Chandler Physical Therapy</h4><p style={{ fontSize: "12px" }}>2020-present</p>
+                                    <p>
+                                        Responsible for assisting physical therapists with non-clinical tasks while caring for individuals
             </p>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-        <h4 style={{ marginTop: "0%"}}>Personal Trainer - Life Time Fitness</h4><p style={{ fontSize: "12px"}}>2015-2020</p>
-            <p>
-            Responsible for assessing needs/wants of clients and creating health and wellness plans based on their goals
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <h4 style={{ marginTop: "0%" }}>Personal Trainer - Life Time Fitness</h4><p style={{ fontSize: "12px" }}>2015-2020</p>
+                                    <p>
+                                        Responsible for assessing needs/wants of clients and creating health and wellness plans based on their goals
             </p>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>
-        <h4 style={{ marginTop: "0%"}}>Assistant Manager - Crust Italian Restaurant</h4><p style={{ fontSize: "12px"}}>2017-2018</p>
-            <p>
-            Primarily acted as a server and bartender with responsibilities associated with being an assistant manager.  
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <h4 style={{ marginTop: "0%" }}>Assistant Manager - Crust Italian Restaurant</h4><p style={{ fontSize: "12px" }}>2017-2018</p>
+                                    <p>
+                                        Primarily acted as a server and bartender with responsibilities associated with being an assistant manager.
             </p>
-        </TimelineContent>
-      </TimelineItem>
-    </Timeline>
-              </Paper>
-        </Grid>
-        <Grid item xs={12} sm={12} md={6}>
-          <Paper className={classes.resumeContents}>
-              <SchoolIcon />
-          <h2>Education Experience</h2>
-          <Timeline style={{marginLeft: "-80%"}}>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-        <h4 style={{ marginTop: "0%"}}>University of Arizona Full-Stack Web Development</h4><p style={{ fontSize: "12px"}}>August 2020 - Feb 2021</p>
-            <p>
-            Studied Full-Stack web development languages and frameworks such as HTML, CSS, Javascript, Express, NodeJS, MySQl, MongoDB, and ReactJS
+                                </TimelineContent>
+                            </TimelineItem>
+                        </Timeline>
+                    </Paper>
+                </Grid>
+                <Grid item xs={12} sm={12} md={6}>
+                    <Paper className={classes.resumeContents}>
+                        <SchoolIcon />
+                        <h2>Education Experience</h2>
+                        <Timeline style={{ marginLeft: "-80%" }}>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <h4 style={{ marginTop: "0%" }}>University of Arizona Full-Stack Web Development</h4><p style={{ fontSize: "12px" }}>August 2020 - Feb 2021</p>
+                                    <p>
+                                        Studied Full-Stack web development languages and frameworks such as HTML, CSS, Javascript, Express, NodeJS, MySQl, MongoDB, and ReactJS
             </p>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-          <TimelineConnector />
-        </TimelineSeparator>
-        <TimelineContent>
-        <h4 style={{ marginTop: "0%"}}>Northern Arizona University, BS Exercise Science</h4><p style={{ fontSize: "12px"}}>2010-2014</p>
-            <p>
-            Received Bachelor's of Science in Exercise Science with a minor in Chemistry.  Graduated with a 3.4 GPA.
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                    <TimelineConnector />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <h4 style={{ marginTop: "0%" }}>Northern Arizona University, BS Exercise Science</h4><p style={{ fontSize: "12px" }}>2010-2014</p>
+                                    <p>
+                                        Received Bachelor's of Science in Exercise Science with a minor in Chemistry.  Graduated with a 3.4 GPA.
             </p>
-        </TimelineContent>
-      </TimelineItem>
-      <TimelineItem>
-        <TimelineSeparator>
-          <TimelineDot />
-        </TimelineSeparator>
-        <TimelineContent>
-        <h4 style={{ marginTop: "0%"}}>Albuquerque Academy</h4><p style={{ fontSize: "12px"}}>2003-2010</p>
-            <p>
-            </p>
-        </TimelineContent>
-      </TimelineItem>
-    </Timeline>
-              </Paper>
-        </Grid>
-      </Grid>
-    </div>
-  );
+                                </TimelineContent>
+                            </TimelineItem>
+                            <TimelineItem>
+                                <TimelineSeparator>
+                                    <TimelineDot />
+                                </TimelineSeparator>
+                                <TimelineContent>
+                                    <h4 style={{ marginTop: "0%" }}>Albuquerque Academy</h4><p style={{ fontSize: "12px" }}>2003-2010</p>
+                                    <p>
+                                    </p>
+                                </TimelineContent>
+                            </TimelineItem>
+                        </Timeline>
+                    </Paper>
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
 
 export default Resume;
